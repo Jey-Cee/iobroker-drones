3893cc865f230098da4b41e34ce7f97ae175fc64
Mic-M <iob.micm@gmail.com>
2020-10-10 14:05:07 +0200

    add Windows, async state creation

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const os = require('os');
+'use strict';
+
+// https://github.com/Jey-Cee/iobroker-drones
+
+//const os = require('os');
 const fs = require('fs');
 const ioClient = require('socket.io-client');
 const exec = require('child_process').exec;
 
 const ns = 'drones';
-const instance = "0";
+const instance = '0';
 let funcs;
 
 //some information
 let arch, hostname, platform, type, distribution, memory, cpu, cores, cpu_speed;
-let ip = '192.168.0.220';
-let port = 8084;
+const ip = '10.10.0.165';
+const port = 9090;
 
 
 fs.open(__dirname + '/lib/system.json', 'r', (err, fd) => {
@@ -33,7 +37,7 @@ fs.open(__dirname + '/lib/system.json', 'r', (err, fd) => {
 });
 
 function readSystem(callback){
-    let sysinfo = fs.readFile(__dirname + '/lib/system.json', 'utf8', (err, data)=>{
+    const sysinfo = fs.readFile(__dirname + '/lib/system.json', 'utf8', (err, data)=>{
         if(err){
             console.log(err);
         }
@@ -48,6 +52,9 @@ function readSystem(callback){
                 case '7':
                     funcs = require(__dirname + '/lib/android_7');
                     break;
+                case 'windows':
+                    funcs = require(__dirname + '/lib/windows');
+                    break;
 
             }
             return('Ready');
@@ -60,7 +67,7 @@ function readSystem(callback){
 }
 
 function sysInfo(data, callback){
-    let json = JSON.parse(data);
+    const json = JSON.parse(data);
     arch = json['arch'];
     hostname = json['hostname'];
     platform = json['platform'];
@@ -79,16 +86,20 @@ process.on('SIGTERM', ()=>{
     process.exit();
 });
 
-let conn = ioClient.connect('http://' + ip + ':' + port, {
-        query:  'key=nokey',
-        'reconnection limit':           10000,
-        'max reconnection attempts':    Infinity
-    });
-
+const conn = ioClient.connect('http://' + ip + ':' + port, {
+    query: 'key=nokey',
+    'reconnection limit': 10000,
+    'max reconnection attempts': Infinity
+});
 
 conn.on('connect', ()=>{
     init();
-})
+});
+
+conn.on('connect_error', (error)=>{
+    console.error(`Error: ${error} - Please check if ip ('${ip}') and port('${port}') matches with socketio adapter settings.`);
+    conn.emit('setState', ns + '.0.' + hostname + '.info.connected', false);
+});
 
 conn.on('error', (error) => {
     console.log('Error: ' + error);
@@ -114,7 +125,7 @@ conn.on('objectChange', (id, obj)=>{
 
 conn.on('stateChange', (id, state)=>{
     console.log(id);
-    let path = ns + '.0.' + hostname + '.';
+    const path = ns + '.0.' + hostname + '.';
     switch(id){
         case path + 'command':
             command(state.val);
@@ -126,61 +137,106 @@ conn.on('stateChange', (id, state)=>{
             break;
         case path + 'reboot':
             if(state.val === true){
-            command('shutdown -r now', (result)=>{
-                if(result === 'Error'){
-                    command('reboot')
-                }
-            });
+                command('shutdown -r now', (result) => {
+                    if (result === 'Error') {
+                        command('reboot');
+                    }
+                });
             }
             break;
         case path + 'audio.mute':
-            mute();
+            // mute();
             break;
-
+        case path + 'hibernate':
+            if (distribution==='windows') command('shutdown -h'); // Windows command
+            break;
     }
-    console.log('stateChange: ' + id + ' ' + state);
-})
+    if (state) console.log('stateChange: ' + id + ' ' + state);
+});
 
 
 
-function init(){
-    conn.emit('name', hostname);
+async function init(){
 
-    conn.emit('subscribe', `${ns}.${instance}.${hostname}*`);
+    try {
 
-    conn.emit('getObject', ns +'.0.' + hostname, (err, data)=>{
-        if(data === null){
-            createObjects();
-        }else{
-            conn.emit('setState', ns +'.0.' + hostname + '.info.connected', {val: true, expire: 51});
-        }
-    })
+        const path = `${ns}.${instance}.${hostname}`;
+
+        conn.emit('name', hostname);
+
+        const data = await getObjectAsync(path);
+        if (data === null) await createObjectsAsync();
+
+        conn.emit('setState', path + '.info.connected', {val: true, expire: 51});
+
+        conn.emit('subscribe', `${path}.*`);
+
+    } catch (error) {
+        console.error(`init() Unexpected error: '${error}'`);
+    }
 
 }
 
-function createObjects(){
-    conn.emit('setObject', ns + '.0.' + hostname, {"type": "device", "common": {"name": hostname, "role": "drone"}, "native": {}} );
-    conn.emit('setObject', ns +'.0.' + hostname + '.info.connected', {"type": "state", "common": {"name": "Connected", "role": "indicator.state", "type": "boolean", "read": true, "write": false}, "native": {}});
-    conn.emit('setObject', ns + '.0.' + hostname + '.command', {"type": "state", "common": {"name": "Command", "role": "indicator.state", "type": "string", "read": true, "write": true}, "native": {}});
-    conn.emit('setObject', ns + '.0.' + hostname + '.shutdown', {"type": "state", "common": {"name": "Shutdown", "role": "indicator.state", "type": "boolean", "read": true, "write": true, "def": false}, "native": {}});
-    conn.emit('setObject', ns + '.0.' + hostname + '.reboot', {"type": "state", "common": {"name": "Reboot", "role": "indicator.state", "type": "boolean", "read": true, "write": true, "def": false}, "native": {}});
-    conn.emit('setObject', ns + '.0.' + hostname + '.cmd_answer', {"type": "state", "common": {"name": "Command", "role": "indicator.state", "type": "string", "read": true, "write": false}, "native": {}});
+async function createObjectsAsync() {
+    const path = `${ns}.${instance}.${hostname}`;
+    await setObjectAsync(path, {'type': 'device', 'common': {'name': hostname, 'role': 'drone'}, 'native': {}} );
+    await setObjectAsync(path + '.info.connected', {'type': 'state', 'common': {'name': 'Connected', 'role': 'indicator.state', 'type': 'boolean', 'read': true, 'write': false}, 'native': {}});
+    await setObjectAsync(path + '.command', {'type': 'state', 'common': {'name': 'Command', 'role': 'indicator.state', 'type': 'string', 'read': true, 'write': true}, 'native': {}});
+    await setObjectAsync(path + '.shutdown', {'type': 'state', 'common': {'name': 'Shutdown', 'role': 'indicator.state', 'type': 'boolean', 'read': true, 'write': true, 'def': false}, 'native': {}});
+    await setObjectAsync(path + '.reboot', {'type': 'state', 'common': {'name': 'Reboot', 'role': 'indicator.state', 'type': 'boolean', 'read': true, 'write': true, 'def': false}, 'native': {}});
+    await setObjectAsync(path + '.cmd_answer', {'type': 'state', 'common': {'name': 'Command', 'role': 'indicator.state', 'type': 'string', 'read': true, 'write': false}, 'native': {}});
+
+    switch(distribution){
+        case 'ubuntu':
+            //
+            break;
+        case 'debian':
+            //
+            break;
+        case '7':
+            //
+            break;
+        case 'windows':
+            await setObjectAsync(path + '.hibernate', {'type': 'state', 'common': {'name': 'Hibernate', 'role': 'button', 'type': 'boolean', 'read': true, 'write': true, 'def': false}, 'native': {}});
+            break;
+    }      
+
 }
 
 
-function command(cmd, callback){
+/**
+ * Execute a command
+ * @param {string} cmd
+ * @param {object} [callback=undefined] 
+ */
+function command(cmd, callback=undefined) {
     exec(cmd, (error, stdout, stderr)=>{
-        if (error){console.log(error)};
+        if (error){console.log(error);}
         if(stdout){
-            let answer = stdout.trim();
+            const answer = stdout.trim();
             console.log('stdout: ' + answer);
             conn.emit('setState', ns +'.0.' + hostname + '.cmd_answer', answer);
         }else if(stderr){
             console.log('stderr: ' + stderr);
-            callback('Error');
+            callback && callback('Error');
             conn.emit('setState', ns +'.0.' + hostname + '.cmd_answer', 'Error: ' + stderr);
         }
     });
 }
 
 
+function getObjectAsync(path) {
+    return new Promise(resolve => {
+        conn.emit('getObject', path, (err, data)=>{
+            resolve(data);
+        });
+    });
+}
+
+function setObjectAsync(path, obj) {
+    return new Promise(resolve => {
+        conn.emit('setObject', path, obj, ()=>{
+            resolve();
+        });
+    });
+}
