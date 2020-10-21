a36e56ee64a37047ee43a95a8a79b0cb75800452
jey-cee <jey-cee@live.com>
2020-10-21 21:42:20 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget "https://github.com/Jey-Cee/iobroker-drones/archive/0.0.1.tar.gz" -O temp.tar.gz
+tar -xzf temp.tar.gz
+rm temp.tar.gz
+mv iobroker-drones-0.0.1 iobroker-drones
+cd iobroker-drones
+npm install
+node main.js
+
