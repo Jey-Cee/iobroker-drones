6fc45870e39013709866bdf69305297e44c3152a
jey-cee <jey-cee@live.com>
2020-10-21 23:34:00 +0200

    fixed link in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://github.com/Jey-Cee/iobroker-drones/archive/0.0.1.tar.gz" -O temp.tar.gz
+wget "https://github.com/Jey-Cee/iobroker-drones/archive/0.0.2.tar.gz" -O temp.tar.gz
 tar -xzf temp.tar.gz
 rm temp.tar.gz
 mv iobroker-drones-0.0.2 iobroker-drones
