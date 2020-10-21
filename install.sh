2f97d4e6d1040996d40bade7a46e13a40899c761
jey-cee <jey-cee@live.com>
2020-10-21 23:31:39 +0200

    change version in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 wget "https://github.com/Jey-Cee/iobroker-drones/archive/0.0.1.tar.gz" -O temp.tar.gz
 tar -xzf temp.tar.gz
 rm temp.tar.gz
-mv iobroker-drones-0.0.1 iobroker-drones
+mv iobroker-drones-0.0.2 iobroker-drones
 cd iobroker-drones
 npm install
 node main.js
